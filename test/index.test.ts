38d2b3c046cf67e24ccb3e254afb28eae3f449b9
chlouzada <chlouzada@gmail.com>
2023-04-13 19:04:37 -0300

    test for ref

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,6 +1,46 @@
-import { test, assert } from "vitest"
-import { foo } from "../src"
+import { test, assert, beforeEach, afterEach, describe } from "vitest"
+import { Client } from "../src"
+import { z } from "zod"
 
-test("simple", () => {
-  assert.equal(foo, "foo")
+const options = {
+  uri: "mongodb://localhost:27017/first",
+  db: {
+    first: {
+      persons: z.object({ title: z.string() }),
+    },
+    second: {
+      animals: z.any(),
+    },
+  },
+}
+
+const client = new Client(options)
+
+const clearDb = async () => {
+  for (const db in options.db) {
+    const dbName = db as keyof typeof options.db
+    await client.db(dbName).dropDatabase()
+  }
+}
+
+beforeEach(async () => {
+  await client.connect()
+  await clearDb()
+})
+
+afterEach(async () => {
+  await clearDb()
+  await client.close()
+})
+
+describe("client testing", () => {
+  test("typed ref key", async () => {
+    const dbName = "first"
+    const collectionName = "persons"
+
+    const collection = client.ref(`${dbName}.${collectionName}`)
+
+    assert.equal(collection.collectionName, collectionName)
+    assert.equal(collection.dbName, dbName)
+  })
 })
