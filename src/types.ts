4cd5bdbf7bec4ddba98b566d8d03cb254d2423e3
chlouzada <chlouzada@gmail.com>
2023-04-30 13:15:36 -0300

    refac types

--- src/types.ts
+++ src/types.ts
@@ -4,15 +4,11 @@ export type Document<T> = T & { _id: ObjectId | string }
 
 export type AnyObject = Record<string, any>
 
-export type Key<T extends AnyObject> = {
-  [K in keyof T & string]: T[K] extends AnyObject
-    ? `${K}.${Nested<T[K]>}`
-    : never
-}[keyof T & string]
-
-export type Nested<T extends AnyObject> = keyof {
-  [K in keyof T & string]: T[K] extends object ? K : never
-}
+export type Key<TObject extends AnyObject> = {
+  [TKeyDb in keyof TObject & string]: `${TKeyDb}.${keyof {
+    [TKeyCollection in keyof TObject[TKeyDb] & string]: TKeyCollection
+  }}`
+}[keyof TObject & string]
 
 export type Collection<T extends AnyObject> = {
   [K in Key<T>]: T[K]
