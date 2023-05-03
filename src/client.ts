7ceb1cfe46145413dcf3b7547701adf7be485c0f
chlouzada <chlouzada@gmail.com>
2023-05-03 18:07:24 -0300

    Revert "remove code for v0.0.1"
    This reverts commit 0f824ade55f3c41cd3c8960d265e3db31a3c0619.

--- src/client.ts
+++ src/client.ts
@@ -3,14 +3,21 @@ import {
   DbOptions,
   MongoClientOptions,
   MongoClient,
+  ObjectId,
+  Collection,
 } from "mongodb"
-import { Key } from "./types"
+import { Key, KeyCollection } from "./types"
+import { z } from "zod"
+import { toObjectId } from "./utils/toObjectId"
+
+export let client: MongoClient
 
 export class Client<
   T extends Record<string, readonly string[]>,
 > extends MongoClient {
   constructor({ uri, options }: { uri: string; options?: MongoClientOptions }) {
     super(uri, options)
+    client = this
   }
 
   ref(
@@ -26,4 +33,92 @@ export class Client<
       options?.dbOptions,
     )
   }
+
+  model<
+    TSchema extends z.ZodTypeAny,
+    TOptions extends {
+      // TODO: restrict to only allow keys of type string / Array<string>
+      refs?: Partial<Record<keyof z.infer<TSchema>, KeyCollection<Key<T>>>>
+    },
+  >(key: Key<T>, schema: TSchema, options?: TOptions) {
+    const [dbName, collectionName] = key.split(".")
+    const collection = this.db(dbName).collection(collectionName)
+    return {
+      findById: findById<Key<T>, TSchema, TOptions>(collection, options),
+      find: find<TSchema>(collection),
+    }
+  }
 }
+
+const find =
+  <TSchema extends z.ZodTypeAny>(collection: Collection) =>
+  async (filter?: z.infer<TSchema>): Promise<z.infer<TSchema>[]> =>
+    collection.find(filter ?? {}).toArray() as unknown as z.infer<TSchema>[]
+
+const findById = <
+  TKey extends `${string}.${string}`,
+  TSchema extends z.ZodTypeAny,
+  TOptions extends {
+    // TODO: restrict to only allow keys of type string / Array<string>
+    refs?: Partial<Record<keyof z.infer<TSchema>, KeyCollection<TKey>>>
+  },
+>(
+  collection: Collection,
+  options?: TOptions,
+) => {
+  const { refs } = options ?? {}
+  function fn<T extends Partial<Record<keyof TOptions["refs"], true>>>(
+    id: string | ObjectId,
+    options: {
+      populate: T
+    },
+  ): Promise<
+    | Document<
+        Omit<z.infer<TSchema>, keyof T> & {
+          [K in keyof TOptions["refs"]]: z.infer<TSchema>[K] extends Array<any>
+            ? unknown[]
+            : unknown
+        }
+      >
+    | undefined
+  >
+  function fn(
+    id: string | ObjectId,
+  ): Promise<Document<z.infer<TSchema>> | undefined>
+  async function fn(): Promise<unknown> {
+    const [id, options] = arguments as unknown as [
+      string | ObjectId,
+      {
+        populate: TOptions["refs"]
+      }?,
+    ]
+    if (options?.populate) {
+      const lookup = Object.keys(options.populate).map((key) => ({
+        $lookup: {
+          // FIXME: could break
+          from: refs![key],
+          localField: key,
+          foreignField: "_id",
+          as: key,
+        },
+      }))
+      const populated = await collection
+        .aggregate([
+          {
+            $match: {
+              _id: toObjectId(id),
+            },
+          },
+          ...lookup,
+        ])
+        .toArray()
+      return populated[0]
+    }
+    return collection.findOne({
+      _id: toObjectId(id),
+    })
+  }
+  return fn
+}
+
+type Document<T> = T & { _id: ObjectId }
